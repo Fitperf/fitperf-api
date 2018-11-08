59e508c1408675ad9319a09b59cb456c83ad6a10
juliennuellas <julien.nuellas@gmail.com>
2018-11-08 15:37:06 +0100

    set up db with docker

--- fitperf_api/settings.py
+++ fitperf_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'fitperf_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fitperf',
+        'USER': 'dev-user',
+        'PASSWORD': 'dev-password',
+        'HOST': '0.0.0.0',
+        'PORT': '15432'
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
