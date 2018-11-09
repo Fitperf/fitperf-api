a83de43f395e8712ff19b21c3275c7177adc5b4c
juliennuellas <julien.nuellas@gmail.com>
2018-11-09 18:43:11 +0100

    some tests on movement settings views had been added

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,3 +1,4 @@
+import json
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -39,8 +40,56 @@ class MovementSettingsTest(APITestCase):
         """
         TestDatabase.create()
 
-    def test_not_connected_get_all_movementsettings(self):
+    def test_not_connected_get_all_movement_settings(self):
+        """
+        Test if, we are not authenticated,  the API returns a 403 status on this request
+        """
         url = reverse('movement_settings_list')
         response = self.client.get(url, format='json')
-        print(response.status_code)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_not_connected_get_one_movement_settings(self):
+        """
+        Test if, we are not authenticated,  the API returns a 403 status on this request
+        """
+        url = reverse('movement_setting_detail', kwargs={'pk': 1})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_admin_get_all_movement_settings(self):
+        """
+        Test if, when we are logged with an admin account, the API returns:
+            - a 200 status on this request
+            - all the movement settings
+        """
+        self.client.login(username='admin_user', password='admin_password')
+        url = reverse('movement_settings_list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 4)
+    
+    def test_admin_get_one_movement_settings(self):
+        """
+        Test if, when we are logged with an admin account, the API returns:
+            - a 200 status on this request
+            - get the adequate movement
+        """
+        self.client.login(username='admin_user', password='admin_password')
+        url = reverse('movement_setting_detail', kwargs={'pk': 1})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'id': 1, 'name': 'repetitions', 'founder': 1})
+
+    def test_admin_create_one_movement_settings(self):
+        """
+        Test if, when we are logged with an admin account, the API creates correctly
+        the movement settings
+        """
+        self.client.login(username='admin_user', password='admin_password')
+        founder = User.objects.get(username='admin_user')
+        initial_movement_settings = MovementSettings.objects.count()
+        url = reverse('movement_settings_list')
+        data = {'name': 'lestes', 'founder': founder.pk}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(MovementSettings.objects.count(), initial_movement_settings + 1)
