66e87802f3d133f6b3ef013910dd0323906b5f26
juliennuellas <julien.nuellas@gmail.com>
2018-11-12 17:38:37 +0100

    unit tests for equipment requests had been done

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -6,10 +6,245 @@ from django.contrib.auth.models import User
 from ..models import Equipment, Movement, MovementSettings
 from .helper_dbtestdata import TestDatabase
 
+class EquipmentTest(APITestCase):
+    """
+    This class will test all the interactions we can have with
+    Equipment views. What will be tested:
+        -> Not Connected:
+            FAIL:
+                -> Get all equipments
+                -> Get one specific equipment
+        -> With admin account:
+            SUCCESS:
+                -> Get all equipments
+                -> Get one specific equipment
+                -> Create a new equipment
+                -> Delete an equipment
+                -> Modify an equipment
+            FAIL:
+                -> Create a new equipment where the name already exists
+        -> With non admin account:
+            SUCCESS:
+                -> Get all the equipments
+                -> Get one specific equipment
+            FAIL:
+                -> Create a new equipment
+                -> Delete a equipment
+                -> Modify a equipment
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        """
+        Create a database for test with TestDatabase helper
+        """
+        TestDatabase.create()
+
+    def test_not_connected_get_all_equipements(self):
+        """
+        Test if, we are not authenticated, the API returns a 403 status on this request
+        """
+        url = reverse('equipments_list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_not_connected_get_one_equipment(self):
+        """
+        Test if, we are not authenticated, the API returns a 403 status on this request
+        """
+        kb = Equipment.objects.get(name="kettlebell")
+        url = reverse('equipment_detail', kwargs={'pk': kb.pk})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_admin_get_all_equipements(self):
+        """
+        Test if, when we are logged with an admin account, the API returns:
+            - a 200 status on this request
+            - all the equipments
+        """
+        self.client.login(username='admin_user', password='admin_password')
+        url = reverse('equipments_list')
+        equipments = Equipment.objects.count()
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), equipments)
+    
+    def test_admin_get_one_equipment(self):
+        """
+        Test if, when we are logged with an admin account, the API returns:
+            - a 200 status on this request
+            - get the adequate equipment
+        """
+        self.client.login(username='admin_user', password='admin_password')
+        kb = Equipment.objects.get(name="kettlebell")
+
+        url = reverse('equipment_detail', kwargs={'pk': kb.pk})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        equipment_response = {
+            'id': kb.pk,
+            'name': kb.name,
+            'founder': kb.founder.pk
+        }
+        self.assertEqual(response.data, equipment_response)
+
+    def test_admin_create_one_equipment(self):
+        """
+        Test if, when we are logged with an admin account, the API creates correctly
+        the equipment
+        """
+        self.client.login(username='admin_user', password='admin_password')
+        founder = User.objects.get(username='admin_user')
+        initial_equipments = Equipment.objects.count()
+        url = reverse('equipments_list')
+        data = {
+            'name': 'box',
+            'founder': founder.pk
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Equipment.objects.count(), initial_equipments + 1)
+
+    def test_admin_create_one_equipment_which_already_exists(self):
+        """
+        Test if, when we are logged with an admin account, the API does not create
+        the equipment because the name is already used in the database
+        """
+        self.client.login(username='admin_user', password='admin_password')
+        founder = User.objects.get(username='admin_user')
+        initial_equipments = Equipment.objects.count()
+        url = reverse('equipments_list')
+        data = {
+            'name': 'kettlebell',
+            'founder': founder.pk
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Equipment.objects.count(), initial_equipments)
+
+
+    def test_admin_update_one_equipment(self):
+        """
+        Test if, when we are logged with an admin account, the API updates correctly
+        the equipment
+        """
+        self.client.login(username='admin_user', password='admin_password')
+        kb = Equipment.objects.get(name="kettlebell")
+        url = reverse('equipment_detail', kwargs={'pk': kb.pk})
+        data = {
+            "id": kb.pk,
+            "name": "big kettlebell",
+            "founder": kb.founder.pk
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {"id": kb.pk, "name": "big kettlebell", "founder": kb.founder.pk})
+
+    def test_admin_delete_one_equipment(self):
+        """
+        Test if, when we are logged with an admin account, the API deletes correctly the equipment
+        """
+        self.client.login(username='admin_user', password='admin_password')
+        initial_equipments = Equipment.objects.count()
+        kb = Equipment.objects.get(name="kettlebell")
+        data = {
+            "id": kb.pk,
+            "name": kb.name,
+            "founder": kb.founder.pk
+        }
+        url = reverse('equipment_detail', kwargs={'pk': kb.pk})
+        response = self.client.delete(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Equipment.objects.count(), initial_equipments - 1)
+
+    def test_non_admin_get_all_equipements(self):
+        """
+        Test if, when we are logged with a non admin account, the API returns:
+            - a 200 status on this request
+            - all the equipments
+        """
+        self.client.login(username='ordinary_user', password='ordinary_password')
+        url = reverse('equipments_list')
+        equipments = Equipment.objects.count()
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), equipments)
+    
+    def test_non_admin_get_one_equipment(self):
+        """
+        Test if, when we are logged with a non admin account, the API returns:
+            - a 200 status on this request
+            - get the adequate equipment
+        """
+        self.client.login(username='ordinary_user', password='ordinary_password')
+        kb = Equipment.objects.get(name="kettlebell")
+
+        url = reverse('equipment_detail', kwargs={'pk': kb.pk})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        equipment_response = {
+            'id': kb.pk,
+            'name': kb.name,
+            'founder': kb.founder.pk
+        }
+        self.assertEqual(response.data, equipment_response)
+
+    def test_non_admin_create_one_equipment(self):
+        """
+        Test if, when we are logged with a non admin account, the API does not create
+        the equipment
+        """
+        self.client.login(username='ordinary_user', password='ordinary_password')
+        founder = User.objects.get(username='ordinary_user')
+        initial_equipments = Equipment.objects.count()
+        url = reverse('equipments_list')
+        data = {
+            'name': 'box',
+            'founder': founder.pk
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Equipment.objects.count(), initial_equipments)
+
+    def test_non_admin_update_one_equipment(self):
+        """
+        Test if, when we are logged with a non admin account, the API does not update
+        the equipment
+        """
+        self.client.login(username='ordinary_user', password='ordinary_password')
+        kb = Equipment.objects.get(name="kettlebell")
+        url = reverse('equipment_detail', kwargs={'pk': kb.pk})
+        data = {
+            "id": kb.pk,
+            "name": "big kettlebell",
+            "founder": kb.founder.pk
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_non_admin_delete_one_equipment(self):
+        """
+        Test if, when we are logged with a non admin account, the API does not delete the
+        equipment
+        """
+        self.client.login(username='ordinary_user', password='ordinary_password')
+        initial_equipments = Equipment.objects.count()
+        kb = Equipment.objects.get(name="kettlebell")
+        data = {
+            "id": kb.pk,
+            "name": kb.name,
+            "founder": kb.founder.pk
+        }
+        url = reverse('equipment_detail', kwargs={'pk': kb.pk})
+        response = self.client.delete(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Equipment.objects.count(), initial_equipments)
+
 class MovementSettingsTest(APITestCase):
     """
     This class will test all the interactions we can have with
-    MovementSettings class. What will be tested:
+    MovementSettings views. What will be tested:
         -> Not Connected:
             FAIL:
                 -> Get all movement's settings
@@ -64,9 +299,10 @@ class MovementSettingsTest(APITestCase):
         """
         self.client.login(username='admin_user', password='admin_password')
         url = reverse('movement_settings_list')
+        settings = MovementSettings.objects.count()
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 4)
+        self.assertEqual(len(response.data), settings)
     
     def test_admin_get_one_movement_settings(self):
         """
@@ -75,10 +311,17 @@ class MovementSettingsTest(APITestCase):
             - get the adequate movement setting
         """
         self.client.login(username='admin_user', password='admin_password')
-        url = reverse('movement_setting_detail', kwargs={'pk': 1})
+        rep = MovementSettings.objects.get(name=MovementSettings.REPETITIONS)
+
+        url = reverse('movement_setting_detail', kwargs={'pk': rep.pk})
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, {'id': 1, 'name': 'repetitions', 'founder': 1})
+        setting_response = {
+            'id': rep.pk,
+            'name': rep.name,
+            'founder': rep.founder.pk
+        }
+        self.assertEqual(response.data, setting_response)
 
     def test_admin_create_one_movement_settings(self):
         """
@@ -154,10 +397,11 @@ class MovementSettingsTest(APITestCase):
             - all the movement settings
         """
         self.client.login(username='ordinary_user', password='ordinary_password')
+        settings = MovementSettings.objects.count()
         url = reverse('movement_settings_list')
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 4)
+        self.assertEqual(len(response.data), settings)
     
     def test_non_admin_get_one_movement_settings(self):
         """
@@ -166,10 +410,17 @@ class MovementSettingsTest(APITestCase):
             - get the adequate movement
         """
         self.client.login(username='ordinary_user', password='ordinary_password')
-        url = reverse('movement_setting_detail', kwargs={'pk': 1})
+        rep = MovementSettings.objects.get(name=MovementSettings.REPETITIONS)
+
+        url = reverse('movement_setting_detail', kwargs={'pk': rep.pk})
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, {'id': 1, 'name': 'repetitions', 'founder': 1})
+        setting_response = {
+            'id': rep.pk,
+            'name': rep.name,
+            'founder': rep.founder.pk
+        }
+        self.assertEqual(response.data, setting_response)
 
     def test_non_admin_create_one_movement_settings(self):
         """
@@ -224,7 +475,7 @@ class MovementSettingsTest(APITestCase):
 class MovementTest(APITestCase):
     """
     This class will test all the interactions we can have with
-    Movement class. What will be tested:
+    Movement views. What will be tested:
         -> Not Connected:
             FAIL:
                 -> Get all movements
@@ -293,11 +544,21 @@ class MovementTest(APITestCase):
             - get the adequate movement
         """
         self.client.login(username='admin_user', password='admin_password')
-        squat = Movement.objects.get(name="squat")
+        squat = Movement.objects.get(name='squat')
+        rep = MovementSettings.objects.get(name=MovementSettings.REPETITIONS)
+        weight = MovementSettings.objects.get(name=MovementSettings.WEIGHT)
         url = reverse('movement_detail', kwargs={'pk': squat.pk})
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, {'id': 5, 'name': 'squat', 'equipment': 5, 'founder': 4, 'settings': [6, 7]})
+
+        movement_response = {
+            'id': squat.pk,
+            'name': squat.name,
+            'equipment': squat.equipment.pk,
+            'founder': squat.founder.pk,
+            'settings': [rep.pk, weight.pk]
+        }
+        self.assertEqual(response.data, movement_response)
 
 
     def test_admin_create_one_movement(self):
@@ -388,8 +649,6 @@ class MovementTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Movement.objects.count(), initial_movements - 1)
 
-##
-
     def test_non_admin_get_all_movements(self):
         """
         Test if, when we are logged with a non admin account, the API returns:
@@ -410,11 +669,21 @@ class MovementTest(APITestCase):
             - get the adequate movement
         """
         self.client.login(username='ordinary_user', password='ordinary_password')
-        squat = Movement.objects.get(name="squat")
+        squat = Movement.objects.get(name='squat')
+        rep = MovementSettings.objects.get(name=MovementSettings.REPETITIONS)
+        weight = MovementSettings.objects.get(name=MovementSettings.WEIGHT)
         url = reverse('movement_detail', kwargs={'pk': squat.pk})
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, {'id': 5, 'name': 'squat', 'equipment': 5, 'founder': 4, 'settings': [6, 7]})
+
+        movement_response = {
+            'id': squat.pk,
+            'name': squat.name,
+            'equipment': squat.equipment.pk,
+            'founder': squat.founder.pk,
+            'settings': [rep.pk, weight.pk]
+        }
+        self.assertEqual(response.data, movement_response)
 
     def test_non_admin_create_one_movement(self):
         """
