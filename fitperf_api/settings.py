f0f2fbf96df75df88cf32245ecf64eca179bd9af
juliennuellas <julien.nuellas@gmail.com>
2018-11-29 16:44:15 +0100

    still a problem with statics

--- fitperf_api/settings.py
+++ fitperf_api/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
